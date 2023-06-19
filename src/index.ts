21d81302f5280bb48da753c1e6115a4dd181a99c
Officialyenum <oponechukwuyenum@gmail.com>
2023-06-19 17:43:03 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
-
-const color: Color = {
-    red: 20,
-    blue: 10,
-    green: 10,
-}
-
-export default color;
+export * from './errors';
+export * from './middlewares';
